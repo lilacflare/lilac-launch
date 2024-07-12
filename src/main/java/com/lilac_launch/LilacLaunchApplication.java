c0913730476051d36a35544a96028fc016332d45
lilacflare <emmasb34@gmail.com>
2024-07-11 22:15:14 -0500

    Initial Commit

---
+++ src/main/java/com/lilac_launch/LilacLaunchApplication.java
@@ -0,0 +1,12 @@
+package com.lilac_launch.lilac_launch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilacLaunchApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LilacLaunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lilac_launch/lilac_launch/LilacLaunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilac_launch.lilac_launch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilacLaunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
